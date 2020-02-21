ed6e2bdcda15c8a6d29f5d719cc9bc2b15f1c64c
wpxdartmouth <61171981+wpxdartmouth@users.noreply.github.com>
2020-02-21 10:30:43 -0500

    firebase1

---
+++ StayFocus/app/src/main/java/edu/dartmouth/stayfocus/FirebaseHelper.java
@@ -0,0 +1,57 @@
+package edu.dartmouth.stayfocus;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import edu.dartmouth.stayfocus.Entry;
+
+
+public class FirebaseHelper {
+
+    private static FirebaseAuth firebaseAuth;
+    private static FirebaseUser firebaseUser;
+
+    private static DatabaseReference database;
+    private static String userId;
+
+    public FirebaseHelper(){
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+
+        database = FirebaseDatabase.getInstance().getReference();
+        userId = firebaseUser.getUid();
+    }
+
+    public void addEntry(Entry entry){
+        database.child("users").child(userId).child("items").push().setValue(entry);
+//        database.child("users").child(userId).child("entries").child(String.valueOf(entry.getId())).child("id").setValue(entry.getId());
+//        database.child("users").child(userId).child("entries").child(String.valueOf(entry.getId())).child("title").setValue(entry.getTitle());
+
+    }
+
+    public void deleteEntry(int position){
+        database.child("users").child(userId).child("items")
+                .orderByChild("title")
+                .equalTo((String) test_list1.listView.getItemAtPosition(position))
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.hasChildren()) {
+                            DataSnapshot firstChild = dataSnapshot.getChildren().iterator().next();
+                            firstChild.getRef().removeValue();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+    }
+}
---
+++ StayFocus/app/src/main/java/edu/dartmouth/stayfocus/LogInActivity.java
@@ -0,0 +1,85 @@
+package edu.dartmouth.stayfocus;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LogInActivity extends AppCompatActivity {
+
+    protected EditText emailEditText;
+    protected EditText passwordEditText;
+    protected Button logInButton;
+    protected TextView signUpButton;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+
+        // Initialize FirebaseAuth
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        emailEditText = (EditText) findViewById(R.id.emailField);
+        passwordEditText = (EditText) findViewById(R.id.passwordField);
+        logInButton = (Button) findViewById(R.id.loginButton);
+        signUpButton = (Button) findViewById(R.id.signUpButton);
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LogInActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                if (email.isEmpty() || password.isEmpty()) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(LogInActivity.this);
+                    builder.setMessage(R.string.login_error_message)
+                            .setTitle(R.string.login_error_title)
+                            .setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                } else {
+                    firebaseAuth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(LogInActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Intent intent = new Intent(LogInActivity.this, test_list1.class);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        startActivity(intent);
+                                    } else {
+                                        AlertDialog.Builder builder = new AlertDialog.Builder(LogInActivity.this);
+                                        builder.setMessage(task.getException().getMessage())
+                                                .setTitle(R.string.login_error_title)
+                                                .setPositiveButton(android.R.string.ok, null);
+                                        AlertDialog dialog = builder.create();
+                                        dialog.show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+    }
+}
